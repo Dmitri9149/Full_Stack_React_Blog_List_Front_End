c4d80a74f26ae7e34da2ac3be2afd84a28857852
dmitri9149 <dmitri.as.name@gmail.com>
2021-02-12 18:02:19 +0200

    display the name of logged user

--- src/App.js
+++ src/App.js
@@ -76,6 +76,9 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      <div>
+        <p>{user.name} logged in</p>
+      </div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
